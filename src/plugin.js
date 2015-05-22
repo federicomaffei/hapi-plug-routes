560ed484765462374cb488689bd87e1ea85c2989
Federico Maffei <federico.maffei@gmail.com>
2015-05-22 15:52:03 +0100

    added ability to read directory from options

--- src/plugin.js
+++ src/plugin.js
@@ -3,14 +3,14 @@
 var fs = require('fs');
 
 exports.register = function(plugin, options, next) {
-    fs.readdir(process.cwd() + '/src/routes', function(routeLookupError, files) {
+    fs.readdir(process.cwd() + options.directory, function(routeLookupError, files) {
         if(routeLookupError) {
             throw routeLookupError;
         }
 
         files.forEach(function(file) {
-            plugin.route(require(process.cwd() + '/src/routes/' + file));
+            plugin.route(require(process.cwd() + options.directory + file));
         });
-        next();
     });
+    next();
 };
